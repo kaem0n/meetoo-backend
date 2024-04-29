c4011be2fde87e61bb615b492331c517bdcc84c5
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-04-29 18:00:12 +0200

    Initial commit

---
+++ src/main/java/kaem0n/meetoo/MeetooApplication.java
@@ -0,0 +1,13 @@
+package kaem0n.meetoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kaem0n/meetoo/MeetooApplicationTests.java
@@ -0,0 +1,13 @@
+package kaem0n.meetoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
