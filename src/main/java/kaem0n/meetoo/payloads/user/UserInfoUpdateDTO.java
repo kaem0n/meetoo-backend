cb0f257a49da69e397db244592876e19dccee887
Antonio Ruggia Piquer <149959739+kaem0n@users.noreply.github.com>
2024-05-14 22:49:49 +0200

    changes validation constraint

--- src/main/java/kaem0n/meetoo/payloads/user/UserInfoUpdateDTO.java
+++ src/main/java/kaem0n/meetoo/payloads/user/UserInfoUpdateDTO.java
@@ -6,9 +6,9 @@ import org.hibernate.validator.constraints.URL;
 
 import java.util.List;
 
-public record UserInfoUpdateDTO(@Size(min = 3, max = 50, message = "Name field length must be between 3 and 50 characters.")
+public record UserInfoUpdateDTO(@Size(max = 50, message = "Name field length must not exceed 50 characters.")
                                 String name,
-                                @Size(min = 3, max = 50, message = "Surname field length must be between 3 and 50 characters.")
+                                @Size(max = 50, message = "Surname field length must not exceed 50 characters.")
                                 String surname,
                                 @Pattern(regexp = "^\\d{4}\\-(0?[1-9]|1[012])\\-(0?[1-9]|[12][0-9]|3[01])$",
                                         message = "Birthday field must follow YYYY-MM-DD format.")
@@ -17,7 +17,7 @@ public record UserInfoUpdateDTO(@Size(min = 3, max = 50, message = "Name field l
                                 String proPicUrl,
                                 @Pattern(regexp = "MALE|FEMALE|UNDEFINED", message = "Invalid gender value (available: MALE, FEMALE, UNDEFINED).")
                                 String gender,
-                                @Size(min = 3, max = 50, message = "Occupation field length must be between 3 and 50 characters.")
+                                @Size(max = 50, message = "Occupation field length must not exceed 50 characters.")
                                 String occupation,
                                 String bio,
                                 List<@Size(min = 3, max = 50, message = "Hobby field length must be between 3 and 50 characters.") String> hobbies) {
